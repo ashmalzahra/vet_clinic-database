dc46a194657cd9eff03bb855a0ab6a38e052eb32
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-04 00:24:58 +0500

    Populate data for performance check

--- queries.sql
+++ queries.sql
@@ -225,3 +225,7 @@ WHERE V.name = 'Maisy Smith'
 GROUP BY species
 ORDER BY animal_frec DESC
 LIMIT 1;
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
